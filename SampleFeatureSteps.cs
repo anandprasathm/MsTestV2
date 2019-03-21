10d9d81daa1cf44aa44a52d78893d72bede268e4
Anand Prasath <47940726+anandprasathm@users.noreply.github.com>
2019-03-22 01:25:04 +0530

    Initial Commit

---
+++ SampleFeatureSteps.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class SampleFeatureSteps
+    {
+        IWebDriver driver;
+
+        [Given(@"I launch Google Search Engine")]
+        public void GivenILaunchGoogleSearchEngine()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+        
+        [Given(@"I have entered the (.*)")]
+        public void GivenIHaveEnteredThe(string SearchResult)
+        {
+            driver.FindElement(By.Name("q")).SendKeys(SearchResult);
+        }
+        
+        [Then(@"I press Enter")]
+        public void ThenIPressEnter()
+        {
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,16 @@
+﻿Feature: Sample Feature
+	
+
+@mytag
+Scenario Outline: Google Search
+	Given I launch Google Search Engine
+	And I have entered the <SearchValue>
+	Then I press Enter
+	
+	Examples: 
+	| SearchValue |
+	| Specflow    |
+	| MsTest      |
+	| SpecRun     |
+	| UnitTest    |
+
